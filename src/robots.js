743dbd0dd286cf3e90bbf356717c3b8e1ace36aa
edske <edskevin0008@gmail.com>
2018-11-02 22:26:06 -0700

    first commit

---
+++ src/robots.js
@@ -0,0 +1,29 @@
+export const robots = [
+
+    {
+        id:1,
+        name: 'Optimus Prime',
+        username: 'ThePrimster',
+        email: 'notacar@gmail.com'
+
+    },
+    {
+        id:2,
+        name: 'R2-D2',
+        username: 'Ted',
+        email: 'droid@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Wall-E',
+        username: 'Wally',
+        email: 'Wally@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Bender',
+        username: 'BenderFromTheFuture',
+        email: 'tincan@gmail.com'
+    }
+
+]
