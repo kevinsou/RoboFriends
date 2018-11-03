a213f8eaf2b5460c7d15fc7808fe0429408aee78
edske <edskevin0008@gmail.com>
2018-11-03 00:07:45 -0700

    Updates

---
+++ precache-manifest.d0d9496222258ff0c409e754211d5569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "9b5c4326f730a0aab343",
+    "url": "/RoboFriends/static/js/main.9b5c4326.chunk.js"
+  },
+  {
+    "revision": "912fb906b9fcc2111d2b",
+    "url": "/RoboFriends/static/js/1.912fb906.chunk.js"
+  },
+  {
+    "revision": "912fb906b9fcc2111d2b",
+    "url": "/RoboFriends/static/css/1.7570c357.chunk.css"
+  },
+  {
+    "revision": "b38729a9a3824a3e8a7a9441cc269942",
+    "url": "/RoboFriends/index.html"
+  }
+];
